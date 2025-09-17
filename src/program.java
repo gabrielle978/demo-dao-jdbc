b50b1fd81be64f38900b5a78a6f040b6e8ccaa64
gabrielle978 <gbassiquete@outlook.com>
2025-09-16 22:14:38 -0300

    reusing instantiation

--- src/program.java
+++ src/program.java
@@ -7,11 +7,11 @@ import java.util.Date;
 
 public class program {
     public static void main(String[] args) {
-        //Department obj = new Department(1, "Books");
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
-        Seller seller = sellerDAO.findById(3);
 
+        System.out.println("=== Test 1: seller findById ===");
+        Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
     }
 }
